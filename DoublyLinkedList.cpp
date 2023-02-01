958490842de5995b9586dfee3f17480f71d8c032
error1405 <umangbooyah20@gmail.com>
2023-02-01 17:56:13 +0530

    insert & display

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <conio.h>
-using namespace std;
+// using namespace std;
 
 class node
 {
@@ -21,12 +21,45 @@ void insert(node *&head, int val)
 {
 	node *newnode = new node(val);
 
-	// if (head == NULL)
+	if (head == NULL)
+	{
+		head = newnode;
+		head->prev = NULL;
+	}
+	else
+	{
+		node *temp = head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = newnode;
+		newnode->prev = temp;
+		newnode->next = NULL;
+	}
+}
+
+void display(node *&head)
+{
+
+	for (node *temp = head; temp != NULL; temp = temp->next)
+	{
+		std::cout << temp->data << " ";
+	}
+	// while (temp->next != NULL)
 	// {
+	// 	temp = temp->next;
 	// }
 }
 
-void main()
+int main()
 {
-	cout << "Hello";
+	node *head = NULL;
+	std::cout << "Doubly Linked List" << std::endl;
+
+	insert(head, 1);
+	insert(head, 2);
+	insert(head, 3);
+
+	display(head);
 }
