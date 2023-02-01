a99e8e8a772c0261cb3ac673079a34def97b96b0
error1405 <umangbooyah20@gmail.com>
2023-02-01 17:17:49 +0530

    version 1

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -2,11 +2,31 @@
 #include <conio.h>
 using namespace std;
 
-// class node
-// {
-// }
+class node
+{
+public:
+	int data;
+	node *prev;
+	node *next;
+
+	node(int val)
+	{
+		data = val;
+		prev = NULL;
+		next = NULL;
+	}
+};
+
+void insert(node *&head, int val)
+{
+	node *newnode = new node(val);
+
+	// if (head == NULL)
+	// {
+	// }
+}
 
 void main()
 {
-  cout << "Hello";
+	cout << "Hello";
 }
